5e3f188f095eebd45f751b61c53ffa98115ff64c
npalacio <npalacio2240@gmail.com>
2017-02-08 21:52:46 -0600

    Revising outputs

--- src/general/Processor.java
+++ src/general/Processor.java
@@ -2,7 +2,6 @@ package general;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
@@ -10,7 +9,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.logging.Logger;
 
 import level1.L1Controller;
 
